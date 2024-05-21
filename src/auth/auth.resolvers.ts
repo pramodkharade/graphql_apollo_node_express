34ea05af07e715ef05559caf9a04bea383b8aeac
Pramod Kharade <pramodkharade@users.noreply.github.com>
2024-05-21 17:05:48 +0530

    fix service issue

--- src/auth/auth.resolvers.ts
+++ src/auth/auth.resolvers.ts
@@ -4,41 +4,41 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { GraphQLError } from "graphql";
 
-export const authResolvers:Resolvers ={
-    Mutation:{
-        async signup(parent, {input}, context){
-                const existingUser = await this.userService.findOneByEmail(input.email);
-                if(existingUser) throw new GraphQLError("User already exist",{extensions:{code:"BAD_REQUEST"}});
-                
-                const user = await userService.create(input);
-                const jwtToken = jwt.sign({
-                    email:input.email,userId: user.id
-                },
+export const authResolvers: Resolvers = {
+    Mutation: {
+        async signup(parent, { input }, context) {
+            const existingUser = await userService.findOneByEmail(input.email);
+            if (existingUser) throw new GraphQLError("User already exists", { extensions: { code: "BAD_REQUEST" } });
+
+            const user = await userService.create(input);
+            const jwtToken = jwt.sign({
+                email: input.email, userId: user.id
+            },
                 process.env.JWT_KEY!,
                 {
-                    expiresIn:"7 days"
+                    expiresIn: "7 days"
                 })
-                return { user, jwt:jwtToken }
+            return { user, jwt: jwtToken }
         },
-        async signin(parent,{input},context){
-            const user = await this.userService.findOneByEmail(input.email);
-            if(user) throw new GraphQLError("Wrong credentials",{extensions:{code:"BAD_REQUEST"}});
+        async signin(parent, { input }, context) {
+            const user = await userService.findOneByEmail(input.email);
+            if (!user) throw new GraphQLError("Wrong credentials", { extensions: { code: "BAD_REQUEST" } });
 
-            const correctPwd = await bcrypt.compare(input.password,user.password)
-            if(!correctPwd) throw new GraphQLError("Wrong credentials",{extensions:{code:"BAD_REQUEST"}});
+            const correctPwd = await bcrypt.compare(input.password, user.password)
+            if (!correctPwd) throw new GraphQLError("Wrong credentials", { extensions: { code: "BAD_REQUEST" } });
             const jwtToken = jwt.sign({
-                email:input.email,userId: user.id
+                email: input.email, userId: user.id
             },
-            process.env.JWT_KEY!,
-            {
-                expiresIn:"7 days"
-            })
-            return { user, jwt:jwtToken }
+                process.env.JWT_KEY!,
+                {
+                    expiresIn: "7 days"
+                })
+            return { user, jwt: jwtToken }
         }
     },
-    Query:{
-        currentUser(parent, {}, context){
-            if(!context.authorized) throw new GraphQLError("not authorized",{extensions:{code:"UNAUTHORIZED"}});
+    Query: {
+        currentUser(parent, {}, context) {
+            if (!context.authorized) throw new GraphQLError("Not authorized", { extensions: { code: "UNAUTHORIZED" } });
 
             return context.currentUser;
         }
