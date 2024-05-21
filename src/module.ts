0d590f3eec881e7271b330308b5e90644e84d5a1
Pramod Kharade <kharade.pramod91@gmail.com>
2024-05-21 11:52:14 +0530

    context generate config

--- src/module.ts
+++ src/module.ts
@@ -1,20 +1,34 @@
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer, ExpressContext } from "apollo-server-express";
 import express, { Application } from "express";
 import { readFileSync } from "fs";
 import http from "http";
+import jwt from "jsonwebtoken";
 import { authResolvers } from "./auth/auth.resolvers";
-import { Resolvers } from "./__generated__/resolvers-types";
+import { JwtPayload, Resolvers } from "./__generated__/resolvers-types";
+
+
+export interface MyContext extends ExpressContext{
+    currentUser: JwtPayload;
+    authorized: boolean;
+}
 
 export class AppModule {
     constructor(public resolvers: Resolvers){}
 
-    async startApollo(): Promise<{httpServer: http.Server, server:ApolloServer}>{
+    async startApollo(): Promise<{httpServer: http.Server, server:ApolloServer<MyContext>}>{
        const typeDefs = readFileSync('schema.graphql',{ encoding:"utf-8"})
        const app: Application = express();  // Explicitly type the app variable
        const httpServer = http.createServer(app);
        const server = new ApolloServer({
             resolvers: this.resolvers,
-            typeDefs:typeDefs
+            typeDefs:typeDefs,
+            context: ({req,res}) =>{
+                if(!req.headers.authorization){
+                    return {currentUser: null, req, authorized: false}
+                }
+                const payload = jwt.verify(req.headers.authorization,process.env.JWT_KEY!)
+                return {currentUser: payload, req, authorized: !!payload}
+            }
         });
 
         await server.start();
